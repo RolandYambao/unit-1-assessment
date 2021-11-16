f40363f2e2e79e5bfc03bffa56210236d8daa541
RolandYambao <rolandyambao@gmail.com>
2021-11-15 17:10:13 -0800

    Updated

--- js/main.js
+++ js/main.js
@@ -2,8 +2,25 @@ const buttonPlus = document.getElementById("plus");
 const buttonMinus = document.getElementById("minus");
 let answer = document.getElementById("answer");
 let input = document.getElementById("input");
+input.value = 1;
 
 function operation() {
+    buttonPlus.addEventListener("mouseover", function() {
+        buttonPlus.style.color = "grey";
+        buttonPlus.style.background = "white";
+    })
+    buttonPlus.addEventListener("mouseleave", function() {
+        buttonPlus.style.color = "white";
+        buttonPlus.style.background = "grey";
+    })
+    buttonMinus.addEventListener("mouseover", function() {
+        buttonMinus.style.color = "grey";
+        buttonMinus.style.background = "white";
+    })
+    buttonMinus.addEventListener("mouseleave", function() {
+        buttonMinus.style.color = "white";
+        buttonMinus.style.background = "grey";
+    })
     buttonPlus.addEventListener("click", function () {
         let answerValue = parseInt(answer.innerText);
         let inputValue = parseInt(input.value);
